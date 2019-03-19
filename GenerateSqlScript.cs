fc1f35cf6e7e5104048d4b087d903a9285d7ff4b
Burak Kaya <200383524@student.georgianc.on.ca>
2019-03-19 14:47:21 -0400

    fixed problems with insert scipt

--- GenerateSqlScript.cs
+++ GenerateSqlScript.cs
@@ -13,7 +13,7 @@ namespace Company.Function
             {
                 foreach (var user in workOrder.RequestDetails.Users)
                 {
-                    scriptLines.Add($"INSERT INTO [dbo].[Process] ([server],[user_name],[role],[action]) VALUES ('{server.Name}','{user.Name}','{workOrder.RequestDetails.Role}','ADD');");
+                    scriptLines.Add($"INSERT INTO [dbo].[Process] ([server],[user_name],[role],[action], [work_order_id]) VALUES ('{server.Name}','{user.Name}','{workOrder.RequestDetails.Role}','ADD', {workOrder.WorkOrderId});");
                 }
             }
 
